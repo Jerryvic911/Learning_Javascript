985bdfd33944750626dd9b5b9aadf10e697e2dd6
jeremiahv <jeremiahv791@gmail.com>
2025-07-17 14:07:52 +0100

    if-else statement added functions

--- If_else_statement.js
+++ If_else_statement.js
@@ -18,15 +18,37 @@
 
 
 //cleaner version of the code using if-else-if statement
-let age = 17;                   
-let isLicense = false;          
+const myage = document.getElementById("inputage")
+const submitButton = document.getElementById("submitBtn")
+const result = document.getElementById("resultText")
 
-if(age < 16){
+
+
+let age;                   
+let isLicense = false;   
+
+submitButton.onclick = function(){
+    age = myage.value;
+    age = Number(age)
+    
+    if(age < 16){
     console.log("You are not eligible to drive.");
+    result.textContent = "You are not eligible to drive.";
 }
 else if(!isLicense){
     console.log("You are eligible to drive, but you do not have a valid driver's license.");
+    result.textContent = "You are eligible to drive, but you do not have a valid driver's license.";;
 }
 else{
     console.log("You are eligible to drive and you have a valid driver's license.");
+    result.textContent = "You are eligible to drive and you have a valid driver's license.";
 }
+}
+
+
+//Used better in react
+// function DrivingStatus({ age, isLicense }) {
+//   if (age < 16) return <p>You are not eligible to drive.</p>;
+//   if (isLicense) return <p>You are eligible to drive, but you do not have a valid driver's license.</p>;
+//   return <p>You are eligible to drive and have a valid driver's license.</p>;
+// }
